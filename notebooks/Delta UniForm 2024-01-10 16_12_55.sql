bc0ba941747e3e011986c231b8936a6fc182dd72
Xavier Armitage <xavier.armitage@databricks.com>
2024-01-12 04:33:01 +0000

    Add Delta UniForm table and NYC Taxi Trips table

--- notebooks/Delta UniForm 2024-01-10 16_12_55.sql
+++ notebooks/Delta UniForm 2024-01-10 16_12_55.sql
@@ -17,11 +17,19 @@ VALUES
 -- COMMAND ----------
 
 CREATE
-OR REPLACE TABLE _persist.default.nyc_taxi_trips USING DELTA LOCATION 's3://databricks-zaxier-persistent-storage/external-location1/nyctaxi/trips' TBLPROPERTIES(
+OR REPLACE TABLE _persist.default.nyc_taxi_trips USING DELTA LOCATION 's3://databricks-zaxier-persistent-storage/external-location1/nyctaxi/trips' 
+TBLPROPERTIES(
   'delta.universalFormat.enabledFormats' = 'iceberg',
   'delta.enableDeletionVectors' = false
+  -- 'delta.columnMapping.mode'='name'
+  -- 'delta.enableIcebergCompatV1'=true
 ) AS
 SELECT
-  *
+  to_unix_timestamp(tpep_pickup_datetime) as tpep_pickup_datetime,
+  to_unix_timestamp(tpep_dropoff_datetime) as tpep_dropoff_datetime,
+  trip_distance,
+  fare_amount,
+  pickup_zip,
+  dropoff_zip
 FROM
   samples.nyctaxi.trips;
