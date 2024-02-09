335e322a22f8889ded5f7859c189d6c3785a671c
Xavier Armitage <xavier.armitage@databricks.com>
2024-02-09 15:19:37 +1100

    Delete persist_terraform_state.sh and add persist_workspace_details_after_apply.sh

--- persist_terraform_state.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-cd my_lakehouse/databricks_infrastructure_deployment
-terraform output --json > ../../workspace_details_sensitive.json
-cd ../..
-
-
---
+++ terraform/workspace/persist_workspace_details_after_apply.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+terraform output --json > ./workspace_details_sensitive.json
+
